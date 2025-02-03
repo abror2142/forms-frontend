4d2b0448d11e2ced4d6b173842c4b1b042990d90
Abrorbek Ismoilov <abror2142@gmail.com>
2025-02-03 09:04:16 +0000

    Navbar design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeModeScript, DarkThemeToggle } from "flowbite-react";
+import { ThemeModeScript } from "flowbite-react";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 import Nav from "@/components/Nav";
